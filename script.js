f00a5e00baa57d2d2d15e87069ebddc136a2ed21
“Hanan” <abdulhanandar@gmail.com>
2023-01-09 22:35:25 +0530

    added fetch functionality to the button

---
+++ script.js
@@ -0,0 +1,27 @@
+const adviceId = document.querySelector(".container__p > span");
+const advice = document.querySelector(".container__quote");
+const adviceBtn = document.querySelector(".dice");
+
+const wait = function () {
+  return new Promise((resolve) => {
+    setTimeout(resolve, 2000);
+  });
+};
+const fetchAdvice = async function () {
+  try {
+    adviceBtn.style.opacity = 0;
+    const response = await fetch("https://api.adviceslip.com/advice");
+    if (!response.ok) {
+      throw new Error(`Error: ${response.status}`);
+    }
+    const { slip: data } = await response.json();
+    adviceId.textContent = data.id;
+    advice.textContent = data.advice;
+    await wait();
+    adviceBtn.style.opacity = 1;
+  } catch (err) {
+    adviceId.textContent = "Error";
+    advice.textContent = `${err.message} Try again!`;
+  }
+};
+adviceBtn.addEventListener("click", fetchAdvice);
